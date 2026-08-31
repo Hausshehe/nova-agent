5ef02a90d13975d5d05801a6b6ee03220bc621a7
Hausshehe <132378237+Hausshehe@users.noreply.github.com>
2026-08-31 16:59:52 +0300

    Restore Nova Accessibility Service observation hook

---
+++ app/src/main/java/com/hausshehe/nova/NovaAccessibilityService.kt
@@ -0,0 +1,33 @@
+package com.hausshehe.nova
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
+
+class NovaAccessibilityService : AccessibilityService() {
+    companion object {
+        @Volatile
+        var instance: NovaAccessibilityService? = null
+            private set
+    }
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        instance = this
+        ObservationStore.update(rootInActiveWindow)
+    }
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        ObservationStore.update(rootInActiveWindow)
+    }
+
+    override fun onInterrupt() = Unit
+
+    fun clickNode(node: AccessibilityNodeInfo): Boolean =
+        node.isClickable && node.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+
+    override fun onDestroy() {
+        if (instance === this) instance = null
+        super.onDestroy()
+    }
+}
