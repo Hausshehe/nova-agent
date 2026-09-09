ec3d2a134097af08c0c7744d5a8c5ef7cd3e81f4
Hausshehe <132378237+Hausshehe@users.noreply.github.com>
2026-09-09 14:49:13 +0300

    feat: export repair validation primitives

--- nova_core/improvement/__init__.py
+++ nova_core/improvement/__init__.py
@@ -2,11 +2,15 @@
 
 from .diagnosis import FailureCategory, FailureDiagnosis, diagnose_run
 from .policy import ImprovementDecision, ImprovementPolicy
+from .repair import RepairCandidate
+from .validation import ValidationPolicy
 
 __all__ = [
     "FailureCategory",
     "FailureDiagnosis",
     "ImprovementDecision",
     "ImprovementPolicy",
+    "RepairCandidate",
+    "ValidationPolicy",
     "diagnose_run",
 ]
