48f6a66d42b70394f33e8fe9b98f1a7213362b4b
Hausshehe <132378237+Hausshehe@users.noreply.github.com>
2026-09-19 01:31:22 +0300

    Initialize persistent Nova tracing at app startup

--- app/src/main/java/com/hausshehe/nova/NovaApplication.kt
+++ app/src/main/java/com/hausshehe/nova/NovaApplication.kt
@@ -5,6 +5,7 @@ import android.app.Application
 class NovaApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        NovaTrace.initialize(this)
         BridgeServer.start(this)
     }
 }
